23f08c09e63d6d807f1a149cd2174f6d01b0dd0d
Tarsis Lima <br.tmvdl@gmail.com>
2025-10-27 12:20:37 -0300

    Add database file

---
+++ src/app/database.js
@@ -0,0 +1,67 @@
+const { randomUUID } = require('crypto')
+const fs = require('fs')
+const path = require('path')
+
+const { logger } = require('./logger.js')
+
+const mkdir = (dir) => fs.mkdirSync(dir, { recursive: true })
+
+class database {
+  dir = '.'
+
+  constructor(dir) {
+    this.dir = dir
+    mkdir(this.dir)
+  }
+
+  in(dir) {
+    logger.log('database.in', { dir })
+
+    return new database(path.resolve(this.dir, dir))
+  }
+
+  newObject(id = randomUUID()) {
+    logger.log('database.newObject', { id })
+
+    return new database_object(this.dir, id.toString())
+  }
+}
+
+class database_object {
+  dir = '.'
+  id = null
+
+  constructor(dir, id) {
+    this.dir = dir
+    this.id = id
+    mkdir(this.getFullDir())
+  }
+
+  getFullDir() {
+    logger.log('database_object.getFullDir', {})
+
+    return path.resolve(this.dir, this.id)
+  }
+
+  getParamName(param = '') {
+    logger.log('database_object.getParamName', { param })
+
+    return path.resolve(this.getFullDir(), param)
+  }
+
+  write(key, value = null) {
+    logger.log('database_object.write', { key, value })
+
+    fs.writeFileSync(this.getParamName(key), value)
+  }
+
+  writeMany(many = {}) {
+    logger.log('database_object.writeMany', { many })
+
+    Object.keys(many).map((key) => {
+      this.write(key, many[key])
+    })
+  }
+}
+
+module.exports = { database, database_object }
