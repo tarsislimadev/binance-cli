7a97213472a587a95a919470bc02641247b6d56c
Tarsis Lima <br.tmvdl@gmail.com>
2025-10-27 11:26:29 -0300

    Add project files

---
+++ src/app/index.js
@@ -0,0 +1,29 @@
+const API_URL = 'https://api2.binance.com/api/v3'
+
+const request = async (method, path, body = null) => {
+  return await fetch(`${API_URL}${path}`, { method, body: method == 'GET' ? null : body }).then(r => r.json())
+}
+
+const path4params = (path, params = {}) => {
+  return `${path}?${Object.keys(params).map((key) => `${key}=${params[key]}`).join('&')}`
+}
+
+const binance = {
+  api: {
+    v3: {
+      ticker: {
+        price: async (symbol) => await request('GET', path4params('/ticker/price', { symbol })),
+        historicalTrades: async (symbol) => await request('GET', path4params('/historicalTrades', { symbol })),
+        klines: async (symbol, interval = '1m') => await request('GET', path4params('/klines', { symbol, interval })),
+      }
+    }
+  }
+}
+
+const run = async () => {
+  const symbol = 'LTCBTC'
+  const klines = await binance.api.v3.ticker.klines(symbol)
+  console.log({ symbol, klines })
+}
+
+run().finally(() => process.exit(0))
